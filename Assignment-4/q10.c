7e798d1d87132c0df16f9d977fedfafe80b1a4ec
Vandana123Maurya <107552530+Vandana123Maurya@users.noreply.github.com>
2022-07-19 06:03:51 -0700

    Add files via upload

---
+++ Assignment-4/q10.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+int i,t,n=5;
+for(i=1;i<=10;i++)
+{
+        t=n*i;
+        printf("%d*%d = %d ",n,i,t);
+        printf("\n");
+}
+getch();
+}
---
+++ Assignment-4/q7.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+int i;
+for(i=1;i<=20;i++){
+    if(i%2==0)
+        printf("%d ",22-i);
+}
+getch();
+}
